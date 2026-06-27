5f0b0a3cc7bc5647d3a093fc127f549d43a1bb99
Cardoso <gacds.0308@gmail.com>
2026-06-27 13:56:34 -0300

    Refactor: Delete folder backend cause I going to use localhost and not PHP. Started API integration.

---
+++ js/api/config.js
@@ -0,0 +1,5 @@
+const API_KEY = 'your_api_key_here'; // Replace with
+
+const BASE_URL = "https://api.themoviedb.org/3";
+    
+const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
---
+++ js/api/endpoints.js
@@ -0,0 +1,9 @@
+const ENDPOINTS = {
+
+    popular: "/movie/popular",
+
+    trending: "/trending/movie/week",
+
+    genres: "/genre/movie/list"
+
+};
